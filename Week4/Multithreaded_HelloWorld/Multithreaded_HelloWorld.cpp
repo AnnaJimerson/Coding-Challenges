4cce0519dd77f79738b8b27bcde45548c4a4b5b5
AnnaJimerson <ajimerson@activision.com>
2022-08-04 16:49:33 -0700

    added more to multithreading challenge

--- Week4/Multithreaded_HelloWorld/Multithreaded_HelloWorld.cpp
+++ Week4/Multithreaded_HelloWorld/Multithreaded_HelloWorld.cpp
@@ -5,17 +5,32 @@
 #include <thread>
 
 static int HeapInt = 0;
+int counter = 0;
+bool run = true;
 
 void HelloWorldButCool() {
-    std::cout << "Hello and good day to you World!" << std::endl;
+    while (run) {
+        counter++;
+        
+        if (counter > 1000) counter = 0;
+    }
 }
 
 int main()
 {
+    char input;
     HeapInt = 5;
-
-    std::cout << "Hello World!\n";
     std::thread Hello(HelloWorldButCool);
+
+    while (run) {
+        std::cout << "Input character to stop counter and hit enter.\n";
+        std::cout << "(q to quit)" << std::endl;
+        std::cin >> input;
+        std::cout << "ThreadCounter: " << counter << std::endl;
+
+        if (input == 'q') run = false;
+    }
+
     Hello.join();
     return 0;
 }
